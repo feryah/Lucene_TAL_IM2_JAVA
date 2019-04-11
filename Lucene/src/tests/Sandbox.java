e0acef60f2858ddc053136ff4e74fe8bdb954ece
Milena <milena.chaine@gmail.com>
2019-04-11 16:43:06 +0200

    merge

--- Lucene/src/tests/Sandbox.java
+++ Lucene/src/tests/Sandbox.java
@@ -3,46 +3,54 @@ package tests;
 //test compatibilité eclipse/git
 
 import java.io.IOException;
-import java.nio.file.Path;
 import java.nio.file.Paths;
 
+import org.apache.lucene.analysis.fr.FrenchAnalyzer;
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.document.Document;
 import org.apache.lucene.index.DirectoryReader;
 import org.apache.lucene.index.IndexReader;
-import org.apache.lucene.index.Term;
 import org.apache.lucene.search.IndexSearcher;
 import org.apache.lucene.search.Query;
 import org.apache.lucene.search.ScoreDoc;
-import org.apache.lucene.search.TermQuery;
 import org.apache.lucene.search.TopDocs;
 import org.apache.lucene.store.Directory;
 import org.apache.lucene.store.FSDirectory;
+import org.apache.lucene.queryparser.classic.ParseException;
+import org.apache.lucene.queryparser.classic.QueryParser;
 
 public class Sandbox {
 
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws IOException, ParseException {
 		
-		// directory where your index is stored
-		Path path = Paths.get("/Users/milena/Documents/Travail/M2TAL/java/newindex");
+        //fichiers à indexer
+        String recettes = "/Users/milena/Documents/Travail/M2TAL/java/newindex";
+         
+        //index de référence
+        //String index_recettes = "/Users/milena/Documents/Travail/M2TAL/java/newindex";
+        
+        //ouvrir index de référence et créer un searcher
+        Directory dir = FSDirectory.open(Paths.get(recettes));
+        
+        IndexReader reader = DirectoryReader.open(dir);
 
-		Directory index = FSDirectory.open(path);
-		IndexReader reader = DirectoryReader.open(index);
-		IndexSearcher searcher = new IndexSearcher(reader);
-		
-		
-		Term t = new Term("contents", "vanille");
+        IndexSearcher searcher = new IndexSearcher(reader);
+        
+        //créer une requête
+        QueryParser qp = new QueryParser("contents", new FrenchAnalyzer());
+        Query query = qp.parse("SEICHE");
+         
+        //chercher dans index
+        TopDocs hits = searcher.search(query, 10);
+        
+        System.out.println(hits.totalHits + " résultats");
 
-		// Get the top 10 docs
-		Query query = new TermQuery(t);
-		TopDocs tops= searcher.search(query, 10);
-		ScoreDoc[] scoreDoc = tops.scoreDocs;
-		System.out.println(scoreDoc.length); 
-		for (ScoreDoc score : scoreDoc){
-		    System.out.println("DOC " + score.doc + " SCORE " + score.score);
-		}
+        for (ScoreDoc doc : hits.scoreDocs) 
+        {
+            Document d = searcher.doc(doc.doc);
+            System.out.println("Path : "+ d.get("path") + ", Score : " + doc.score);
+        }
 
-		// Get the frequency of the term
-		int freq = reader.docFreq(t);
-		System.out.println("FREQ " + freq);
 	}
 
 }
