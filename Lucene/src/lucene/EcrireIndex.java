c71976c0904521c6fa9f2db55be4aaaee9b0d288
Milena <milena.chaine@gmail.com>
2019-04-11 14:09:23 +0200

    basics indexwriter

--- Lucene/src/lucene/EcrireIndex.java
+++ Lucene/src/lucene/EcrireIndex.java
@@ -1,34 +1,39 @@
 package lucene;
 
 import java.io.IOException;
-import java.io.InputStream;
-import java.nio.file.FileVisitResult;
-import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.nio.file.SimpleFileVisitor;
-import java.nio.file.attribute.BasicFileAttributes;
 
 import org.apache.lucene.analysis.Analyzer;
 import org.apache.lucene.analysis.fr.FrenchAnalyzer;
-import org.apache.lucene.analysis.standard.StandardAnalyzer;
-import org.apache.lucene.document.Document;
-import org.apache.lucene.document.Field.Store;
-import org.apache.lucene.document.LongPoint;
-import org.apache.lucene.document.StringField;
-import org.apache.lucene.document.TextField;
 import org.apache.lucene.index.IndexWriter;
 import org.apache.lucene.index.IndexWriterConfig;
 import org.apache.lucene.index.IndexWriterConfig.OpenMode;
-import org.apache.lucene.index.Term;
 import org.apache.lucene.store.Directory;
 import org.apache.lucene.store.FSDirectory;
 
 public class EcrireIndex {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws IOException {
+		
+        //répertoire des fichiers à indexer
+        String recettes = "/Users/milena/Documents/Travail/M2TAL/java/recettes-utf-8";
+         
+        //répertoire qui contiendra l'index lucene
+        String indexRecettes = "/Users/milena/Documents/Travail/M2TAL/java/index-recettes";
+ 
+        //chemin vers l'index
+        final Path indexChemin = Paths.get(recettes);
+        Directory dir = FSDirectory.open(indexChemin);
+        
+        //analyseur
+        Analyzer analyseur = new FrenchAnalyzer();
+         
+        //configurer l'indexWriter qui va indexer les fichiers
+        IndexWriterConfig indexConfig = new IndexWriterConfig(analyseur);
+        indexConfig.setOpenMode(OpenMode.CREATE_OR_APPEND);
+         
+        IndexWriter writer = new IndexWriter(dir, indexConfig);
 
 	}
-
 }
